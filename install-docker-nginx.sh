62d511a65693ced347bb915b222351127ccba13b
ich <18469758+ichswarox@users.noreply.github.com>
2023-02-28 11:12:03 +0800

    Update install-docker-nginx.sh

--- install-docker-nginx.sh
+++ install-docker-nginx.sh
@@ -4,4 +4,4 @@ curl -sSL https://get.daocloud.io/docker | sh
 &&sudo systemctl restart docker
 &&docker volume create portainer_data && sudo docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 &&cd /root && curl -sSL https://gitee.com/uof9p-xh9-xx-dj/docker-compose/raw/main/docker-compose-nginx.yml > docker-compose.yml
-&&  && docker-compose up -d
+&&cd /root && docker-compose up -d
